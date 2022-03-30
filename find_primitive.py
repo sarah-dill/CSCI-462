a8ccd1142f2dc7a1c41a9f9d30954ebdb8c90a5b
sarah-dill <sed6452@rit.edu>
2022-03-30 10:28:01 -0400

    modified to improve speed

--- find_primitive.py
+++ find_primitive.py
@@ -8,19 +8,22 @@ def main():
     n = int(n)
 
     is_primitive = True
+    cnt = 0
 
     for i in range(2, n):
         for j in range(1, n):
-            ans = pow(i, j) % n
+            ans = pow(i, j, n)
             if ans == 1 and not (j == n-1):
                 is_primitive = False
                 break
             if j == n-1 and not (ans == 1):
                 is_primitive = False
+                break
             if j == n-1 and ans == 1:
                 is_primitive = True
         if is_primitive:
+            cnt += 1
             print(str(i) + " is primitive")
-
+    print("Number primitive elements: " + str(cnt))
 
 main()
